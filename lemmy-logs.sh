06c0efad5dd0c5d479499de736b5bdef14c70978
neutron37 <neutron37@satori.us>
2022-11-29 19:34:53 +0100

    Convenience wrapper for lemmy logs.

---
+++ lemmy-logs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+if [ -z ${1+x} ]; then
+    echo "Must provide an evironment name as argument."
+    exit 1
+fi
+source $1.env
+if ! command -v docker-compose &> /dev/null; then
+    docker compose logs -f
+else
+    docker-compose logs -f
+fi
